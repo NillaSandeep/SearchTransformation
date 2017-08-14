a75f80a63c5f51cd6f57886d30317802cadd557e
Sandeep Nilla <sandeep.nillaibm.com@sandeeps-air.dfw.ibm.com>
2017-08-14 10:42:12 -0500

    Initial commit for SearchTransformation project

---
+++ src/main/java/com/ibm/aa/SearchTransformationApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.aa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchTransformationApplication.class, args);
+	}
+}
