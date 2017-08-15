67b57c8c504585a1bbe3d5deb72e330339b82a97
Sandeep Nilla <sandeep.nillaibm.com@sandeeps-air.dfw.ibm.com>
2017-08-15 10:39:08 -0500

    Added end points for get and set data

--- src/main/java/com/ibm/aa/controller/SearchController.java
+++ src/main/java/com/ibm/aa/controller/SearchController.java
@@ -1,14 +1,35 @@
 package com.ibm.aa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.aa.repository.LinksRepository;
 
 @RestController
 public class SearchController {
+	@Autowired
+	LinksRepository linksRepository;
 	
-	@RequestMapping("/")
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String canaryMethod() {
 		return "This is a test";
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value="/getData", method=RequestMethod.GET)
+	public String retrieveData() {
+		return "JSONDATA";
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value="/setData", method=RequestMethod.POST)
+	public String postData(String jsonData) {
+		linksRepository.saveAndFlush(jsonData);
+		return "Success";
 	}
 	
 }
---
+++ src/main/java/com/ibm/aa/model/LinkDetails.java
@@ -0,0 +1,6 @@
+package com.ibm.aa.model;
+
+public class LinkDetails {
+	private long id;
+	
+}
